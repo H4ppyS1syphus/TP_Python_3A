465718f00c213e12892f66469d927992c284bb71
H4ppyS1syphus <137160829+H4ppyS1syphus@users.noreply.github.com>
2024-10-25 13:40:26 +0200

    First Commit

---
+++ IRM_Cleaned/config.py
@@ -0,0 +1,37 @@
+# config.py
+
+import os
+
+# Directories
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MASKS_DIR = os.path.join(BASE_DIR, 'masks')
+LOGS_DIR = os.path.join(BASE_DIR, 'logs')
+
+# File Paths
+ORIGINAL_MRI_PATH = os.path.join('mri_data', 'csf2.tif')  # Original MRI
+DENOISED_MRI_PATH = os.path.join('mri_data', 'denoised_nl_2.tif')  # Denoised MRI
+DENOISED_TIFF_PATH = os.path.join('mri_data', 'denoised_nl_2.tif')  # Path to save/load denoised TIFF
+
+# Masks
+MASK_FILENAMES = [
+    'csf_mask_class_0_refined_2.tif',
+    'csf_mask_class_1_refined_2.tif',
+    'csf_mask_class_2_refined_2.tif',
+    'csf_mask_class_3_refined_2.tif',
+    'csf_mask_class_4_refined_2.tif'
+]
+
+# Visualization
+OUTPUT_HTML = os.path.join(BASE_DIR, 'mri_csf_visualization_2.html')
+OUTPUT_SLICES_DIR = os.path.join(BASE_DIR, 'slices')
+
+# K-Means Parameters
+K = 5  # Number of clusters
+BATCH_SIZE = 10000  # For MiniBatchKMeans
+
+# Morphological Operations
+STRUCTURING_ELEMENT_RADIUS = 1  # Radius for structuring element
+MIN_SIZE = 1  # Minimum size for small object removal
+
+# Logging
+DEFAULT_LOG_FILE = os.path.join(LOGS_DIR, 'execution.log')
